297ec1511ea4c2e2adbe7dbd4b47fe02618a2bdf
thiago <thiagosoares111@yahoo.com.br>
2023-04-08 11:24:39 -0300

    configuração do tailwind

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Home from './src/components/Home/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className='mt-5 '>
       <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
