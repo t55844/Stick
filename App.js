62d33dfa8d1cff46e01e629e7b9693de8e64dae3
thiago <thiagosoares111@yahoo.com.br>
2023-04-08 14:30:11 -0300

    configuração de rotas e navegação

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
 import Home from './src/components/Home/Home';
+import FormLogin from './src/components/Form/FormLogin/FormLogin';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+
+function App() {
   return (
-    <View className='mt-5 '>
-      <StatusBar style="auto" />
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="login" component={FormLogin} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+
   );
 }
 
-
+export default App;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,8 +10,6 @@ export default function Home() {
             <Text className="bg-primary-500 text-white">This is a primary color</Text>
             <Text className="bg-secondary-500 text-white">This is a secondary color</Text>
             <Text className="bg-neutral-500 text-white">This is a neutral color</Text>
-            <Text className="bg-success-500 text-white">This is a success color</Text>
-            <Text className="bg-danger-500 text-white">This is a danger color</Text>
             <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae nobis voluptates necessitatibus sint sequi earum excepturi! Similique nisi harum, minus sapiente necessitatibus labore sed, quam delectus quod ab, cumque voluptate!</Text>
         </View >
     )
